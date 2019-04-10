e9a7d0b2e7de385febde6218f0a17c6dc87f3465
Louis Poirier <louis.poirier.14@gmail>
2019-04-10 02:03:08 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,93 @@
+let teamList = teams;
+let teamsInPool = [];
+let draftPool = [];
+let draftResults = [];
+let drawCounter = 0;
+window.onload = () => {
+	teamList.forEach(item => {
+		teamsInPool.push(item.abreviation);
+	});
+	displayRankings();
+	populatePool();
+}
+function displayRankings(){
+	let listContainer = document.getElementById("draftList");
+	teamList.forEach((item, index) => {
+		listContainer.innerHTML += 
+			"<div class='rItem'>"+
+				"<h4 class='tItem tRank'>"+ (index+1) +"</h4>"+
+				"<img class='icon tItem' src='./logos/"+item.abreviation+".gif' />"+
+				"<h4 class='tItem'>"+item.name+"</h4>"+
+				"<div class='odds tItem'> Odds: "+item.odds+"%</div>"+
+			"</div>"
+	})
+}
+function populatePool(){
+	teamList.forEach((item, index) => {
+		let entries = item.odds*2;
+		for(i = 0; i<entries; i++){
+			draftPool.push(item.abreviation);
+		}
+	});
+	console.log('Pool size: '+draftPool.length);
+	console.log(draftPool);
+}
+function startDraft(){
+	document.getElementById('preDraft').classList.add('hide');
+	document.getElementById('draftStart').classList.remove('hide');
+}
+function draw(){
+	drawCounter++;
+	if(drawCounter <= 3){
+		let drawResult = Math.floor(Math.random()*draftPool.length);
+		let winner = draftPool[drawResult];
+		console.log('And the winner is '+winner.toUpperCase());
+		draftPool = draftPool.filter(item => {
+			return item !== winner;
+		});
+		teamsInPool = teamsInPool.filter(item => {
+			return item !== winner;
+		});
+		let team = teamList.find(t => t.abreviation == winner)
+		draftResults.push(team);
+
+		document.getElementById(drawCounter).innerHTML = 
+			"<img class='winningTeam' src='./logos/"+winner+".gif' />"
+
+	}else if(drawCounter == 4){
+		endDraft();
+	}
+}
+function endDraft(){
+	let element = document.getElementById("finalRankingContainer");
+	teamsInPool.forEach(item => {
+		let team = teamList.find(t => t.abreviation == item)
+		draftResults.push(team);
+	});
+	draftResults.forEach((item, index) => {
+		element.innerHTML += 
+			"<div class='finalItem'>"+(index+1)+". "+item.city+" "+item.name+"</div>"
+	})
+	element.innerHTML +=
+		"<button class='btn' onclick='saveDraft()'>Enregistrer</button>"
+	console.log(draftResults);
+}
+function saveDraft(){
+	var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(printResults()));
+  element.setAttribute('download', 'repechageSHL2019.txt');
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+function printResults(){
+	let results = "";
+	draftResults.forEach((item, index) => {
+		results += (index+1)+'. '+item.city+' '+item.name+'\r'; 
+	});
+	return results;
+}
---
+++ teams.js
@@ -0,0 +1,107 @@
+let teams = [
+	{
+		name: "Canucks",
+		city: "Vancoucer",
+		abreviation: "van",
+		ranking: 1,
+		odds: 18.5
+	},
+	{
+		name: "Maple Leafs",
+		city: "Toronto",
+		abreviation: "tor",
+		ranking: 2,
+		odds: 13.5
+	},
+	{
+		name: "Sabres",
+		city: "Buffalo",
+		abreviation: "buf",
+		ranking: 3,
+		odds: 11.5
+	},
+	{
+		name: "Red Wings",
+		city: "Detroit",
+		abreviation: "det",
+		ranking: 4,
+		odds: 9.5
+	},
+	{
+		name: "Devils",
+		city: "New Jersey",
+		abreviation: "njd",
+		ranking: 5,
+		odds: 8.5
+	},
+	{
+		name: "Senators",
+		city: "Ottawa",
+		abreviation: "ott",
+		ranking: 6,
+		odds: 7.5
+	},
+	{
+		name: "Bruins",
+		city: "Boston",
+		abreviation: "bos",
+		ranking: 7,
+		odds: 6.5
+	},
+	{
+		name: "Blue Jackets",
+		city: "Columbus",
+		abreviation: "cbj",
+		ranking: 8,
+		odds: 6
+	},
+	{
+		name: "Capitals",
+		city: "Washington",
+		abreviation: "was",
+		ranking: 9,
+		odds: 5
+	},
+	{
+		name: "Avalanche",
+		city: "Colorado",
+		abreviation: "col",
+		ranking: 10,
+		odds: 3.5
+	},
+	{
+		name: "Flames",
+		city: "Calgary",
+		abreviation: "cgy",
+		ranking: 11,
+		odds: 3
+	},
+	{
+		name: "Ducks",
+		city: "Anaheim",
+		abreviation: "ana",
+		ranking: 12,
+		odds: 2.5
+	},
+	{
+		name: "Golden Knights",
+		city: "Vegas",
+		abreviation: "vgk",
+		ranking: 13,
+		odds: 2
+	},
+	{
+		name: "Kings",
+		city: "Los Angeles",
+		abreviation: "lak",
+		ranking: 14,
+		odds: 1.5
+	},
+	{
+		name: "Blues",
+		city: "St-Louis",
+		abreviation: "stl",
+		ranking: 15,
+		odds: 1
+	}
+]
